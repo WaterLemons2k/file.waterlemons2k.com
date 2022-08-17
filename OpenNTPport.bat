9c66e474d2e112f6e1752eb636d165a3492b3495
WaterLemons2k <62788816+WaterLemons2k@users.noreply.github.com>
2022-08-17 17:32:37 +0800

    Add files via upload

--- OpenNTPport.bat
+++ OpenNTPport.bat
@@ -1,5 +1,5 @@
 @echo off
-::获取管理员 Get Administrator
+::Get Administrator
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (
 echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
@@ -8,6 +8,6 @@ echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
 exit /B
 ) else (
 if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
-::开放NTP端口 Open NTP port
+::Open NTP port
 netsh firewall add portopening UDP 123 "123UDP"
 )
